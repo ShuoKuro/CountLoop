d51793485866a2a4d38d07b8272e9ce1b45a32e1
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 14:15:09 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 21;
+    let mut count: i32 = 0;
+    while x >= 0 {
+        count += 1;
+        x -= 3;
+    }
+    println!("{}", count);
 }
